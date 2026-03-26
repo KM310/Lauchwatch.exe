81f6989b223125a2577ae27c570305c5c3e4fa22
K_M <julius@marcel-loos.de>
2026-03-26 20:24:30 +0100

    Refactor WebView2 initialization and handlers

--- exe/main.cpp
+++ exe/main.cpp
@@ -5,57 +5,85 @@ HWND g_hWnd = nullptr;
 ICoreWebView2Controller* g_controller = nullptr;
 ICoreWebView2* g_webview = nullptr;
 
-LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
+// ---------------------------
+// Callback-Klasse 1: Environment
+// ---------------------------
+class EnvCompletedHandler :
+    public ICoreWebView2CreateCoreWebView2EnvironmentCompletedHandler
 {
-    switch (msg)
+public:
+    ULONG STDMETHODCALLTYPE AddRef() override { return 1; }
+    ULONG STDMETHODCALLTYPE Release() override { return 1; }
+    HRESULT STDMETHODCALLTYPE QueryInterface(REFIID riid, void** ppv) override
     {
-    case WM_SIZE:
-        if (g_controller)
+        if (riid == __uuidof(IUnknown) ||
+            riid == __uuidof(ICoreWebView2CreateCoreWebView2EnvironmentCompletedHandler))
         {
-            RECT r;
-            GetClientRect(hWnd, &r);
-            g_controller->put_Bounds(r);
+            *ppv = this;
+            return S_OK;
         }
-        break;
-
-    case WM_DESTROY:
-        PostQuitMessage(0);
-        break;
+        *ppv = nullptr;
+        return E_NOINTERFACE;
     }
 
-    return DefWindowProc(hWnd, msg, wParam, lParam);
-}
+    HRESULT STDMETHODCALLTYPE Invoke(HRESULT hr, ICoreWebView2Environment* env) override
+    {
+        env->CreateCoreWebView2Controller(
+            g_hWnd,
+            new ControllerCompletedHandler()
+        );
+        return S_OK;
+    }
 
-void InitWebView2()
-{
-    CreateCoreWebView2EnvironmentWithOptions(
-        nullptr, nullptr, nullptr,
-        Callback<ICoreWebView2CreateCoreWebView2EnvironmentCompletedHandler>(
-            [](HRESULT hr, ICoreWebView2Environment* env) -> HRESULT
+    // ---------------------------
+    // Callback-Klasse 2: Controller
+    // ---------------------------
+    class ControllerCompletedHandler :
+        public ICoreWebView2CreateCoreWebView2ControllerCompletedHandler
+    {
+    public:
+        ULONG STDMETHODCALLTYPE AddRef() override { return 1; }
+        ULONG STDMETHODCALLTYPE Release() override { return 1; }
+        HRESULT STDMETHODCALLTYPE QueryInterface(REFIID riid, void** ppv) override
+        {
+            if (riid == __uuidof(IUnknown) ||
+                riid == __uuidof(ICoreWebView2CreateCoreWebView2ControllerCompletedHandler))
             {
-                env->CreateCoreWebView2Controller(
-                    g_hWnd,
-                    Callback<ICoreWebView2CreateCoreWebView2ControllerCompletedHandler>(
-                        [](HRESULT hr, ICoreWebView2Controller* controller) -> HRESULT
-                        {
-                            g_controller = controller;
-
-                            controller->get_CoreWebView2(&g_webview);
-
-                            RECT r;
-                            GetClientRect(g_hWnd, &r);
-                            controller->put_Bounds(r);
-
-                            g_webview->Navigate(L"https://lauchwatch.base44.app/");
-
-                            return S_OK;
-                        }
-                    ).Get()
-                );
+                *ppv = this;
                 return S_OK;
             }
-        ).Get()
-    );
+            *ppv = nullptr;
+            return E_NOINTERFACE;
+        }
+
+        HRESULT STDMETHODCALLTYPE Invoke(HRESULT hr, ICoreWebView2Controller* controller) override
+        {
+            g_controller = controller;
+            controller->get_CoreWebView2(&g_webview);
+
+            RECT r;
+            GetClientRect(g_hWnd, &r);
+            controller->put_Bounds(r);
+
+            g_webview->Navigate(L"https://lauchwatch.base44.app/");
+            return S_OK;
+        }
+    };
+};
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+    if (msg == WM_SIZE && g_controller)
+    {
+        RECT r;
+        GetClientRect(hWnd, &r);
+        g_controller->put_Bounds(r);
+    }
+
+    if (msg == WM_DESTROY)
+        PostQuitMessage(0);
+
+    return DefWindowProc(hWnd, msg, wParam, lParam);
 }
 
 int WINAPI WinMain(HINSTANCE hInst, HINSTANCE, LPSTR, int)
@@ -73,7 +101,6 @@ int WINAPI WinMain(HINSTANCE hInst, HINSTANCE, LPSTR, int)
     wc.hInstance = hInst;
     wc.lpszClassName = L"LauchWatchWindow";
     wc.hIcon = hIcon;
-
     RegisterClass(&wc);
 
     g_hWnd = CreateWindow(
@@ -87,7 +114,11 @@ int WINAPI WinMain(HINSTANCE hInst, HINSTANCE, LPSTR, int)
 
     ShowWindow(g_hWnd, SW_SHOW);
 
-    InitWebView2();
+    // WebView2 starten
+    CreateCoreWebView2EnvironmentWithOptions(
+        nullptr, nullptr, nullptr,
+        new EnvCompletedHandler()
+    );
 
     MSG msg;
     while (GetMessage(&msg, nullptr, 0, 0))
@@ -98,4 +129,3 @@ int WINAPI WinMain(HINSTANCE hInst, HINSTANCE, LPSTR, int)
 
     return 0;
 }
-
